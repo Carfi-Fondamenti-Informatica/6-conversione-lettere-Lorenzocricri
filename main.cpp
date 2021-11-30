d06c60c88eb2d6948313bcf18d17e8e866bdeb24
Lorenzocricri <91842642+Lorenzocricri@users.noreply.github.com>
2021-11-30 22:48:07 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,12 @@
 #include <iostream>
-#include "lib.h"
+#include "cmake-build-debug/lib.h"
 using namespace std;
 
 int main() {
     char x=0;
     char valore=0;
-    cout << "Inserire un carattere a scelta" << endl;
     cin >> x;
-    if(x>= 'A' and x<= 'Z'){
-        cout << "vero" << endl;
-        valore = cambio(x);
-        cout << valore << endl;
-    }else if(x>= 'a' and x<= 'z'){
+    if(controllo(x) == true){
         cout << "vero" << endl;
         valore = cambio(x);
         cout << valore << endl;
