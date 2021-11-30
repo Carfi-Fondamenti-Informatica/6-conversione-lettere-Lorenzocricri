ac3942b0ecbea90ede2a6a34ef06180f3b5c5ba5
Lorenzocricri <91842642+Lorenzocricri@users.noreply.github.com>
2021-11-30 22:48:57 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 #include "lib.h"
 
+bool controllo(char a){
+    if(a>= 'A' and a<= 'Z'){
+        return true;
+    }else if(a>= 'a' and a<= 'z'){
+        return true;
+    }else{
+        return false;
+    }
+}
+
 char cambio(char a) {
     if (a >= 'A' and a <= 'Z') {
         a = a + 32;
