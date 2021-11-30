ddde40c5598d90386e3f08ebdcc9bc1894526133
Lorenzocricri <91842642+Lorenzocricri@users.noreply.github.com>
2021-11-30 22:19:57 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,8 +6,13 @@ using namespace std;
 int main() {
     char x=0;
     char valore=0;
+    cout << "Inserire un carattere a scelta" << endl;
     cin >> x;
-    if(controllo(x) == true){
+    if(x>= 'A' and x<= 'Z'){
+        cout << "vero" << endl;
+        valore = cambio(x);
+        cout << valore << endl;
+    }else if(x>= 'a' and x<= 'z'){
         cout << "vero" << endl;
         valore = cambio(x);
         cout << valore << endl;
