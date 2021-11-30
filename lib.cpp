ed7db298b8b8df7553849939838b7bf1443049af
Lorenzocricri <91842642+Lorenzocricri@users.noreply.github.com>
2021-11-30 22:06:06 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include "primaLibreria.h"
 
+bool controllo(char a){
+    if(a>= 'A' and a<= 'Z'){
+        return true;
+    }else if(a>= 'a' and a<= 'z'){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+char cambio(char a) {
+    if (a >= 'A' and a <= 'Z') {
+        a = a + 32;
+    } else if (a >= 'a' and a <= 'z'){
+        a = a - 32;
+    }
+    return a;
+}
